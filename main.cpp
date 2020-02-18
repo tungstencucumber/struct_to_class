8aa5b0c40f51799c71eef16ab4311b7894a2ddac
tungstencucumber <44pyriaiev44@gmail.com>
2020-02-18 23:55:14 +0300

    Fuctions are reshaped for better interaction with the container (class "Container" was renamed to "Vehicle" in order to match the setting).

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class vessel
+class Vehicle
 {
 public:
-	vessel(int i);
-	//~vessel();
-	vessel* attach_to_fleet(vessel* ship);
-	vessel* skew();
-	vessel* split();
+    // Виртуальные методы, должны быть реализованы вашим контейнером
+    virtual void create(int id) = 0;
+    virtual bool exists(int id) = 0;
+    virtual void remove(int id) = 0;
+
+    // И этот тоже, хотя к нему потом ещё вернёмся
+    virtual void report() = 0;
+    
+    // Виртуальный деструктор (пока просто поверьте, что он нужен)
+    virtual ~Vehicle() { };
+};
+
+class Vessel
+{
 private:
 	int id;
 	int level;
-	vessel* parent;
-	vessel* left;
-	vessel* right;
+	Vessel* parent;
+	Vessel* left;
+	Vessel* right;
+public:
+	Vessel(int i);
+	//~Vessel();
+	Vessel* attach_to_fleet(Vessel* ship);
+	Vessel* skew();
+	Vessel* split();
+	bool find_vessel(int i);
 };
 
-vessel::vessel(int i)
+Vessel::Vessel(int i)
 {
 	this->id = i;
 	this->level = 0;
@@ -26,7 +42,7 @@ vessel::vessel(int i)
 	this->right = NULL;
 }
 
-vessel* vessel::attach_to_fleet(vessel* ship)
+Vessel* Vessel::attach_to_fleet(Vessel* ship)
 {
 	if(this->id > ship->id) 
 	{
@@ -50,11 +66,11 @@ vessel* vessel::attach_to_fleet(vessel* ship)
 	this->split();
 }
 
-vessel* vessel::skew()
+Vessel* Vessel::skew()
 {
 	if (this->left != NULL)
 	if (this->level == this->left->level) {
-		vessel* k = this->left;
+		Vessel* k = this->left;
 		if (this->parent != NULL)
 		{
 			k->parent = this->parent;
@@ -72,13 +88,13 @@ vessel* vessel::skew()
 	return this;
 }
 
-vessel* vessel::split()
+Vessel* Vessel::split()
 {
 	if(this->right != NULL)
 	if(this->right->right != NULL)
 	if(this->right->right->level == this->level)
 	{
-		vessel* r = this->right;
+		Vessel* r = this->right;
 		if (this->parent != NULL) {
 			r->parent = this->parent;
 			this->parent = r;
@@ -97,32 +113,95 @@ vessel* vessel::split()
 }
 
 
-class fleet
+bool Vessel::find_vessel(int i) {
+	if (this->id == i)
+		return true;
+	if (this->id > i && this->left != NULL)
+		this->left->find_vessel(i);
+	if (this->id < i && this->right != NULL)
+		this->right->find_vessel(i);
+	return false;
+}
+
+Vessel* Vessel::detach_from_fleet(Vessel* ship) {
+	if (ship->id < this->id)
+		this->left->detach_from_fleet(ship);
+	else if (ship->id > this->id)
+		this->right->detach_from_fleet(ship);
+	else {
+		if (this->left == NULL) {
+			if (this->parent != NULL) {
+				this->parent->right = this->right;
+				if (this->right != NULL)
+					this->right->parent = this->parent;
+			}
+			delete this;
+		} else {
+			vessel *replaced = this->left;
+			while (replaced->right != NULL)
+				replaced = replaced->right;
+			vessel* t = replaced->parent;
+			this->id = replaced->id;
+			replaced->parent->right = NULL;
+			delete replaced;
+			while(t != this) {
+				t = skew(t);
+				t = split(t);
+				t = t->parent;
+			}
+		}
+	}
+	return this;
+}
+
+
+class Fleet: public Vehicle
 {
 public:
-	fleet(vessel* r);
+	Fleet(Vessel* r);
 	//~fleet();
-	void attach_to_fleet(vessel* ship);
+	void attach_to_fleet(Vessel* ship);
+	bool find_vessel(int i);
+	void detach_from_fleet(Vessel* ship);
+
+	void create(int value) {
+		Vessel t(value) = new Vessel;
+		this->attach_to_fleet(t);
+	}
+
+    bool exists(int value) {
+    	return this->find_vessel(value);
+    }
+
+    void remove(int value) {
+    	this->root->detach_from_fleet(value);
+    }
+
 private:
-	vessel* root;
+	Vessel* root;
 };
 
-fleet::fleet(vessel* r)
+Fleet::Fleet(Vessel* r)
 {
 	this->root = r;
 }
 
-void fleet::attach_to_fleet(vessel* ship)
+void Fleet::attach_to_fleet(Vessel* ship)
 {
 	this->root = root->attach_to_fleet(ship);
 }
 
+bool Fleet::find_vessel(int i)
+{
+	return this->root->find_vessel(i);
+}
+
+void Fleet::detach_from_fleet(Vessel* ship) {
+	if (this->find_vessel(ship) == true)
+		this->root = root->detach_from_fleet(ship);
+}
 
 int main() {
-	vessel* normandy = new vessel(10);
-	vessel* luna = new vessel(5);
-	vessel* enterprise = new vessel(15);
-	fleet my_fleet(normandy);
-	my_fleet.attach_to_fleet(luna);
+
 	return 0;
 }
